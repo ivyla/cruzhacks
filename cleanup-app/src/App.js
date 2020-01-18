a55f9016dc838c13b4a813c6c3acb27de5eb371e
Dylan Cirimelli-Low <cirimellilowdylan@gmail.com>
2020-01-18 12:04:28 -0800

    Radar.io SDK integration

--- cleanup-app/src/App.js
+++ cleanup-app/src/App.js
@@ -3,6 +3,7 @@ import { Layout } from 'antd'
 
 import Header from './components/Header'
 import Sider from './components/Sider'
+import Radar from 'radar-sdk-js'
 
 const { Content, Footer } = Layout
 
@@ -11,6 +12,20 @@ const { Content, Footer } = Layout
 const App = () => {
   const [isCollapse, setCollapse] = useState(true)
   return (
+
+    <script src="https://js.radar.io/v1.1.0/radar.min.js" integrity="sha384-9gzMIZTQDnuwordlQUv+j9r2TDNLAaKXhSk+p/1jZV0tlnDQ8Bu6YVmTbLIG+dRg" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+      Radar.initialize(publishableKey);
+      Radar.trackOnce(function(status, location, user, events) {
+        if (status === Radar.STATUS.SUCCESS) {
+          if (user.place.chain.slug === 'walmart') {
+            // do something
+          }
+        }
+      });
+    </script>
+
     <Layout className="layout">
       <Sider isCollapse={isCollapse}></Sider>
       <Layout>
