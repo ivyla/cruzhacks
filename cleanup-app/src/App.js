ed75fd94129560a5d7a5ec614d049b9aa0b1046a
Art Parkeenvincha <art.parkeenvincha@gmail.com>
2020-01-18 12:24:36 -0800

    radar is in!

--- cleanup-app/src/App.js
+++ cleanup-app/src/App.js
@@ -1,31 +1,25 @@
 import React, { useState } from 'react'
 import { Layout } from 'antd'
+import Radar from 'radar-sdk-js'
 
 import Header from './components/Header'
 import Sider from './components/Sider'
-import Radar from 'radar-sdk-js'
 
 const { Content, Footer } = Layout
 
-// ALL JAVASCRIPT CODE
+Radar.trackOnce((status, location, user, events) => {
+  if (status === Radar.STATUS.SUCCESS) {
+    for (const geofence in user.geofences) {
+      if (geofence.tag === 'stadium') {
+        // do something
+      }
+    }
+  }
+})
 
 const App = () => {
   const [isCollapse, setCollapse] = useState(true)
   return (
-
-    <script src="https://js.radar.io/v1.1.0/radar.min.js" integrity="sha384-9gzMIZTQDnuwordlQUv+j9r2TDNLAaKXhSk+p/1jZV0tlnDQ8Bu6YVmTbLIG+dRg" crossorigin="anonymous"></script>
-
-    <script type="text/javascript">
-      Radar.initialize(publishableKey);
-      Radar.trackOnce(function(status, location, user, events) {
-        if (status === Radar.STATUS.SUCCESS) {
-          if (user.place.chain.slug === 'walmart') {
-            // do something
-          }
-        }
-      });
-    </script>
-
     <Layout className="layout">
       <Sider isCollapse={isCollapse}></Sider>
       <Layout>
