7be9a061a4075331ae7e89c118e63d3a683a771b
Art Parkeenvincha <art.parkeenvincha@gmail.com>
2020-01-18 16:06:54 -0800

    map markers

--- cleanup-app/src/components/MapPage.js
+++ cleanup-app/src/components/MapPage.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 
-const AnyReactComponent = ({ text }) => (
-  <img src="https://img.icons8.com/color/48/000000/marker.png" />
+const greatPlaceStyle = {
+  position: 'absolute',
+  transform: 'translate(-50%, -100%)',
+}
+
+const Marker = () => (
+  <img
+    style={greatPlaceStyle}
+    src="https://img.icons8.com/color/20/000000/marker.png"
+  />
 )
+
+const Markers = () => {
+  return [
+    <Marker lat={37.453639} lng={-122.445115} />,
+    <Marker lat={36.971454} lng={-121.952722} />,
+    <Marker lat={36.963261} lng={-122.009431} />,
+    <Marker lat={36.549343} lng={-121.929567} />,
+    <Marker lat={37.00413} lng={-122.185773} />,
+  ]
+}
 
 const MapPage = () => {
   const defaultProps = {
     center: {
-      lat: 59.95,
-      lng: 30.33,
+      lat: 36.962043,
+      lng: -122.007636,
     },
-    zoom: 11,
+    zoom: 13,
+  }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      defaultProps.center.lat = position.coords.latitude
+      defaultProps.center.lng = position.coords.longitude
+    })
   }
   return (
     <div>
@@ -21,8 +45,13 @@ const MapPage = () => {
           bootstrapURLKeys={{ key: 'AIzaSyDVbQIZYWFU5zfuxRW1Ogt-9GAnmuznuwc' }}
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
+          yesIWantToUseGoogleMapApiInternals
         >
-          <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+          <Marker lat={37.453639} lng={-122.445115} />
+          <Marker lat={36.971454} lng={-121.952722} />
+          <Marker lat={36.963261} lng={-122.009431} />
+          <Marker lat={36.549343} lng={-121.929567} />
+          <Marker lat={37.00413} lng={-122.185773} />
         </GoogleMapReact>
       </div>
     </div>
