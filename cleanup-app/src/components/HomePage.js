6b1c2838cd3f755fbad96f738cd3fef7109bc10a
Ivy <ivyla3@gmail.com>
2020-01-18 23:21:32 -0800

    fixed lnading page, edited some scss files and in the ant - fixed overflow issues, created text, imported img, and more

--- cleanup-app/src/components/HomePage.js
+++ cleanup-app/src/components/HomePage.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import Logo from '../assets/images/cleanup.png'
 
 const HomePage = () => {
-  return <h1>HOME PAGE</h1>
+  return (
+    <div class="wrapper">
+      <div class="centered">
+        <img src={Logo} alt="poo poo pp" id="landing-img" />
+        <div class="yee-haw">
+          <h1 id="title-blurb">
+            {' '}
+            Crowdfund and organize environmental cleanups anywhere in the world.{' '}
+          </h1>
+          <h2 id="description">
+            {' '}
+            We're here to connect environmentally conscious donors and eager
+            volunteers, to clean public places in our communities.{' '}
+          </h2>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
